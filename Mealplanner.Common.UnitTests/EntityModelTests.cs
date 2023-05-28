6677aa71613790ef91a5d5130d9ed9eba5e5c137
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-05-28 14:30:39 +0200

    Added unit test for adding and deleting meals

--- Mealplanner.Common.UnitTests/EntityModelTests.cs
+++ Mealplanner.Common.UnitTests/EntityModelTests.cs
@@ -15,4 +15,37 @@ public class EntityModelTests
             Assert.True(db.Database.CanConnect());
         }
     }
+
+    [Fact]
+    public void AddMealTest()
+    {
+        using (MealplannerContext db = new())
+        {
+            // Create db if it doesn't exist.
+            db.Database.EnsureCreated();
+
+            Meal meal = new("Test Meal", "lunch", "test, test, test");
+            db.Meals.Add(meal);
+            db.SaveChanges();
+
+            Assert.True(db.Meals.Any(m => m.MealName == "Test Meal"));
+        }
+    }
+
+    [Fact]
+    public void DeleteMealTest()
+    {
+        using (MealplannerContext db = new())
+        {
+            // Delete the Test Meal.
+            IQueryable<Meal> meals = db.Meals.Where(m => m.MealName == "Test Meal");
+            if (meals.Any())
+            {
+                db.Meals.RemoveRange(meals);
+                db.SaveChanges();
+            }
+
+            Assert.False(db.Meals.Any(m => m.MealName == "Test Meal"));
+        }
+    }
 }
